9b691fe6dc4b101a568d1d9e9e9858322bdd98ba
javier castillo <jarrilel@gmail.com>
2025-08-11 17:08:12 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,9 +4,12 @@ function setup() {
   background(0,255,180);
 }
 
+//cambiar configuración de color
+colorMode(HSB,360,100,100)
+
 function draw() {
   d = random(10,60)
-  fill(255,random(55,200));
+  fill(random(0,360),100,100);
   ellipse(mouseX,mouseY,d,d);
 }
 
