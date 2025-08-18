f067886eae7fc5268fe06431f7faa26e44cea35b
javier castillo <jarrilel@gmail.com>
2025-08-18 17:48:54 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,10 +5,10 @@ function setup() {
 }
 
 function draw() {
-  d = random(10,60)
+  d = random(10,60);
   
 //cambiar configuración de color
-  colorMode(HSB,360,100,100)
+  colorMode(HSB,360,100,100);
   
   fill(random(0,360),100,100);
   ellipse(mouseX,mouseY,d,d);
@@ -19,3 +19,4 @@ function keyPressed() {
     save('dibujo.jpg');
   }
 }
+
